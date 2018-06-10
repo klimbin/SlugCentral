8ffa119faaa50b2babae8492c2495053f956136f
Filips PC <fhaltmay@ucsc.edu>
2018-06-09 19:00:39 -0700

    Adding the new structure and making the search work

---
+++ app/src/main/java/com/example/kevin/slugcentral/ClassInfo.java
@@ -0,0 +1,13 @@
+package com.example.kevin.slugcentral;
+
+public class ClassInfo {
+    String class_id;
+    String class_name;
+    String date_time;
+    String descriptive_link;
+    String enrolled;
+    String instructor;
+    String link_sources;
+    String location;
+    String status;
+}
