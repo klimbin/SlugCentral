65c0ea4b4b4fa00413354980f5647a5c56be5021
Kevin Lim <klim560@gmail.com>
2018-06-09 21:14:26 -0700

    added detail activity, added filip's failed push w/ working search

--- app/src/main/java/com/example/kevin/slugcentral/SearchActivity.java
+++ app/src/main/java/com/example/kevin/slugcentral/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.kevin.slugcentral;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,7 +11,20 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
@@ -18,24 +32,73 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
     ListView list;
     ListViewAdapter adapter;
     SearchView editSearch;
-    String[] courseNameList;
+    ClassInfo[] courseNameList;
     ArrayList<Course> arrayList = new ArrayList<Course>();
+    //This changes each time the server is put up since we are using the free version which gives
+    //us a new url each time.
+    String tURL = "https://01b94458.ngrok.io/api/v1.0/courses/all/2000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        //https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-        courseNameList = new String[]{"CMPS 121 - Mobile Applications", "CMPE 107 - Probability and Statistics for Engineers", "CMPE 16 - Discrete Math",
-                "AMS 147", "CMPS 130", "CMPS 102", "CMPE 110", "CMPS 183",
-                "CMPS 115"};
+        StrictMode.setThreadPolicy(policy);
+        //https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+
+
+        try {
+            //https://stackoverflow.com/questions/4328711/read-url-to-string-in-few-lines-of-java-code?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+            String jsonString = new Scanner(new URL(tURL).openStream(), "UTF-8").useDelimiter("\\A").next();
+
+            JSONArray jsonarray = new JSONArray(jsonString);
+
+            ClassInfo[] tempArray = new ClassInfo[jsonarray.length()];
+
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                ClassInfo temp = new ClassInfo();
+                temp.class_id = jsonobject.getString("class_id");
+                temp.class_name = jsonobject.getString("class_name");
+                temp.date_time = jsonobject.getString("date_time");
+                temp.descriptive_link = jsonobject.getString("descriptive_link");
+                temp.enrolled = jsonobject.getString("enrolled");
+                temp.instructor = jsonobject.getString("instructor");
+                temp.link_sources = jsonobject.getString("link_sources");
+                temp.location = jsonobject.getString("location");
+                temp.status = jsonobject.getString("status");
+                tempArray[i] = temp;
+
+            }
+            courseNameList = tempArray;
+        }
+        catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
 
         // Locate the ListView in list_view_item.xml
         list = (ListView) findViewById(R.id.listview);
         list.setVisibility(View.INVISIBLE);
 
         for (int i = 0; i < courseNameList.length; i++) {
-            Course course = new Course(courseNameList[i]);
+            Course course = new Course(courseNameList[i].class_name);
             // Binds all strings into an array
             arrayList.add(course);
         }
