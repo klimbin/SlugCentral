8f570716d37c2998211839396833490e9e3d2c5e
twong40 <thomaswong1000@gmail.com>
2018-06-29 19:32:20 -0700

    Feedback needs some server backend

--- app/src/main/java/com/example/kevin/slugcentral/FeedbackActivity.java
+++ app/src/main/java/com/example/kevin/slugcentral/FeedbackActivity.java
@@ -9,12 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class FeedbackActivity extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class FeedbackActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         final Context context = this;
         Button send = findViewById(R.id.send_feedback);
         EditText feedback = findViewById(R.id.feedback_text);
@@ -23,6 +28,9 @@ public class FeedbackActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(FeedbackActivity.this,"Thanks for the feedback <3",Toast.LENGTH_LONG).show();
                 //pop the activity off the stack
+                Bundle bundle = new Bundle();
+                bundle.putString("value", "feedback");
+                mFirebaseAnalytics.logEvent("event", bundle);
                 Intent i = new Intent(context, Directory.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(i);
